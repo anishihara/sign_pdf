9c227200ed480c8e4b36a85f1f5ef1be646a34a1
Anderson Nishihara <nishihara.gmail.com>
2021-04-06 00:42:39 -0300

    Commit inicial

--- sign_pdf/MainWindow.xaml.cs
+++ sign_pdf/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,45 @@ namespace sign_pdf
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void selectPrivateKeyButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Certificado (*.pfx)|*.pfx";
+            if (openFileDialog.ShowDialog() == true) this.privateKeyPathTextBox.Text = openFileDialog.FileName;
+        }
+
+        private void selectPDFButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if(this.privateKeyPathTextBox.Text == "")
+                {
+                    MessageBox.Show("Selecione um certificado!");
+                    return;
+                }
+                if (this.privateKeyPassword.Password == "")
+                {
+                    MessageBox.Show("Digite a senha do certificado!");
+                    return;
+                }
+                string privateKeyFilePath = this.privateKeyPathTextBox.Text;
+                string privateKeyPassword = this.privateKeyPassword.Password;
+                string pdfFilePath = null;
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "PDF (*.pdf)|*.pdf";
+                if (openFileDialog.ShowDialog() == true) pdfFilePath = openFileDialog.FileName;
+                var pdfSigner = new PDFSigner();
+                var pdfFilePathParts = System.IO.Path.GetFileName(pdfFilePath).Split('.');
+                string destinationFilePath = System.IO.Path.GetDirectoryName(pdfFilePath) + "\\" + pdfFilePathParts[0] + "_assinado.pdf";
+                pdfSigner.Sign(pdfFilePath, destinationFilePath, privateKeyFilePath, privateKeyPassword, "", "");
+                MessageBox.Show("PDF assinado com sucesso! A cópia assinada encontra-se na mesma pasta do pdf original.");
+            }
+            catch(Exception exp){
+                MessageBox.Show("Houve um erro durante a assinatura. Verifique se a senha do certificado está correto.", "Erro durante a assinatura", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
---
+++ sign_pdf/PDFSigner.cs
@@ -0,0 +1,49 @@
+﻿using iText.Kernel.Pdf;
+using iText.Signatures;
+using Org.BouncyCastle.Pkcs;
+using Org.BouncyCastle.X509;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace sign_pdf
+{
+    class PDFSigner
+    {
+        public void Sign(string sourceDocument, string destinationPath, string privateKeyPath, string keyPassword, string reason, string location)
+        {
+            using (FileStream privateKeyStream = new FileStream(privateKeyPath, System.IO.FileMode.Open))
+            {
+                Pkcs12Store pk12 = new Pkcs12Store(privateKeyStream, keyPassword.ToCharArray());
+                privateKeyStream.Dispose();
+
+                //then Iterate throught certificate entries to find the private key entry
+                string alias = null;
+                foreach (string tAlias in pk12.Aliases)
+                {
+                    if (pk12.IsKeyEntry(tAlias))
+                    {
+                        alias = tAlias;
+                        break;
+                    }
+                }
+                var pk = pk12.GetKey(alias).Key;
+                var ce = pk12.GetCertificateChain(alias);
+                var chain = new X509Certificate[ce.Length];
+                for (int k = 0; k < ce.Length; ++k)
+                    chain[k] = ce[k].Certificate;
+                // reader and stamper
+                PdfReader reader = new PdfReader(sourceDocument);
+                FileStream fout = new FileStream(destinationPath, FileMode.Create, FileAccess.ReadWrite);
+                StampingProperties properties = new StampingProperties();
+                PdfSigner signer = new PdfSigner(reader, fout, properties);
+
+                PdfSignatureAppearance appearance = signer.GetSignatureAppearance().SetReason(reason).SetLocation(location);
+                IExternalSignature pks = new PrivateKeySignature(pk, "SHA-512");
+                signer.SignDetached(pks, chain, null, null, null, 0, PdfSigner.CryptoStandard.CMS);
+            }
+        }
+    }
+}
